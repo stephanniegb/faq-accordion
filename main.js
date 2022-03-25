91914239204976dd108dac14b6e0afd6a0d992f7
Stephanie Ezinne <stephanieegbuonu@gmail.com>
2022-03-25 23:02:13 +0100

    better commit ;(

--- main.js
+++ main.js
@@ -3,5 +3,10 @@ const accordionHeader = document.querySelectorAll(".accordion-header");
 accordionHeader.forEach(accordionHeader=> {
     accordionHeader.addEventListener("click", event => { 
        accordionHeader.classList.toggle("active");
+       const currentlyActiveAccordionHeader = document.querySelector(".accordion-header.active");
+       if(currentlyActiveAccordionHeader && currentlyActiveAccordionHeader !==accordionHeader){
+           currentlyActiveAccordionHeader.classList.toggle("active");
+           currentlyActiveAccordionHeader.nextSibling.style.maxHeight = 0;
+       }
     });
 });
